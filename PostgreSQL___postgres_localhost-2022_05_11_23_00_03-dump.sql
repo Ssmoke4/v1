f983d876dffd1eb743da900361d54225325772b6
Smoke <you@example.com>
2022-05-11 23:04:13 +0600

    DB DUMP

---
+++ PostgreSQL___postgres_localhost-2022_05_11_23_00_03-dump.sql
@@ -0,0 +1,76 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 14.2 (Ubuntu 14.2-1.pgdg20.04+1+b1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.user_id_seq OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: subscribers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.subscribers (
+    id bigint DEFAULT nextval('public.user_id_seq'::regclass) NOT NULL,
+    balance integer
+);
+
+
+ALTER TABLE public.subscribers OWNER TO postgres;
+
+--
+-- Data for Name: subscribers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.subscribers (id, balance) FROM stdin;
+1	2000
+\.
+
+
+--
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.user_id_seq', 1, true);
+
+
+--
+-- Name: subscribers subscribers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.subscribers
+    ADD CONSTRAINT subscribers_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
